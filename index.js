5a6d3f3c26e64d89b1d86b58cea2a891672851b2
TrentBradshaw <TrentHarrisonBradshaw@gmail.com>
2021-03-02 18:36:26 -0800

    init commit

--- index.js
+++ index.js
@@ -4,39 +4,8 @@ const client = new discord.Client();
 client.once('ready', () => {
 	console.log('Ready!');
 });
+
 email  = process.env.email
-password = process.env.password
-//server = discord.Server(id='816461543113359380')
+password = process.env.password 
+token = process.env.token
 
-client.on('message', message => {
-	console.log(message.content);
-});
-
-client.login(email, password)
-//client.run(email, password)
-/*
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
- 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
-});
-
-client = discord.Client()
-
-
-
-client.on(
-    'ready', () => {
-        print('Logged in as')
-        print(client.user.name)
-        print(client.user.id)
-        print('------')
-        print('get all channel a client belong to ')
-    })
-    
-
-*/
